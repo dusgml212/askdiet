75333a412e4c2b60f66b5aad16065a505597a5fe
moonjh <moonjhyun0613@gmail.com>
2024-04-07 23:12:42 +0900

    update_build.gradle
    fix :
    spring version 2.7.8
    java 1.8

---
+++ src/main/java/com/team5/askdiet/AskdietApplication.java
@@ -0,0 +1,13 @@
+package com.team5.askdiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskdietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskdietApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team5/askdiet/AskdietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.askdiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskdietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
